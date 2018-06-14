16e324a35f206214685649bc8bbfa53386e8d141
ram-chaurasiya <40237558+ram-chaurasiya@users.noreply.github.com>
2018-06-14 11:10:41 +0530

    Demo

---
+++ TestJenkinSetup/TestJenkinSetup/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TestJenkinSetup
+//
+//  Created by Ram Mohan Chaurasiya on 13/06/18.
+//  Copyright © 2018 Ram Mohan Chaurasiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    @IBAction func sidemenuTapped(_ sender: Any) {
+        print("Puzzle button tapped...")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
